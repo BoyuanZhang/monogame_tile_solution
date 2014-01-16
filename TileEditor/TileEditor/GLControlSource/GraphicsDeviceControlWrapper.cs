986fb515f2ecab3f529654868db93e29c1ffcdd9
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-15 20:08:18 -0500

    OpenTK GL Control for WinForms

---
+++ TileEditor/TileEditor/GLControlSource/GraphicsDeviceControlWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TileEditor.GLControlSource
+{
+    //So... really weird windows forms bug that does not allow you to use controls that are abstracting first first level inheritance.
+    //To get around this, we implement a "middle abstraction", because controls that abstract from second level inheritance and onwards work.
+    public abstract class GraphicsDeviceControlWrapper : GraphicsDeviceControl
+    {
+    }
+}
