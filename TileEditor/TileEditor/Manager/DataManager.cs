c0ea9aaa3ea589f0a8d3a8f30df32d27243e9d8a
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-15 20:07:49 -0500

    Data Managers

---
+++ TileEditor/TileEditor/Manager/DataManager.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+
+using Microsoft.Xna.Framework.Graphics;
+
+using TileEditor.Handlers;
+using TileEditor.Utility;
+
+namespace TileEditor.Manager
+{
+    public class DataManager
+    {
+        //Different handlers for the different data that the form will contain (textures, images, layer files etc... )
+        private TextureHandler m_textureHandler;
+        private TileLayerHandler m_tileLayerHandler;
+        private TexturePreviewHandler m_texturePreviewHandler;
+
+        //outline texture for the editor
+        public Texture2D EditorOutlineTexture;
+
+        public DataManager( GraphicsDevice graphicsDevice, string contentPath ) 
+        {
+            m_textureHandler = new TextureHandler(graphicsDevice);
+            m_tileLayerHandler = new TileLayerHandler();
+            m_texturePreviewHandler = new TexturePreviewHandler();
+
+            //initialize any content the data manager will need (textures etc...)
+
+            //hard-coded in path ending for now... come up with a better solution for finding the outline texture afterwards
+            EditorOutlineTexture = Texture2D.FromStream(graphicsDevice, File.OpenRead( contentPath + "/" + "TileEditor/tile_outline.png"));
+        }
+
+        //take in parameters as string and attempt to parse them, or put defaults in if input not in correct format
+        //return false if layer could not be handled (layer name already exists etc...)
+        public bool HandleNewLayer(string layerName, string layerWidth, string layerHeight)
+        {
+            //Parse parameters before giving to layer handler
+            int width = 0;
+            int height = 0;
+            try
+            {
+                width = int.Parse(layerWidth);
+                height = int.Parse(layerHeight);
+
+                //Setting the max / minimum map parameters
+                if (width > 0)
+                    width = Math.Min(width, 80);
+                else
+                    width = 10;
+
+                if (height > 0)
+                    height = Math.Min(height, 80);
+                else
+                    height = 10;
+            }
+            catch (FormatException)
+            {
+                //Defaults to width of 10 and height of 10 if an invalid values are input
+                width = 10;
+                height = 10;
+            }
+
+            //If the new layer is correctly added into data management then return true
+            if (m_tileLayerHandler.HandleNewLayer(layerName, width, height))
+                return true;
+
+            return false;
+        }
+
+        public void RemoveSelectedLayers(List<string> layerNames)
+        {
+            foreach (string name in layerNames)
+            {
+                m_tileLayerHandler.RemoveLayer(name);
+            }
+        }
+
+        //Layer editing handlers
+
+        //Edit draw data
+        public void EditLayerDataDraw(bool fill, string textureKey)
+        {
+            //Let texture handler see if this texture exists, if it does get it and pass it to the layer handler to update current layout
+            if( m_textureHandler.ContainsTexture( textureKey ))
+                m_tileLayerHandler.DrawToLayer(fill, m_textureHandler.GetTexture( textureKey) );
+        }
+
+        //Erase draw data
+        public void EditLayerDataErase(bool fill)
+        {
+            m_tileLayerHandler.EraseFromLayer(fill);
+        }
+
+        //change alpha channel of top most selected layer
+        public void ChangeLayerAlpha(float alpha)
+        {
+            m_tileLayerHandler.SetAlpha(alpha);
+        }
+
+        public string HandleTexture(string textureFileName)
+        {
+            //Call texture handler to manage the texture... should probably either validate here or the form that the texture is in the right path.
+            try
+            {
+                if (m_textureHandler.HandleFile(textureFileName))
+                {
+                    //If texture handler is successful then add the image file to the preview data, if it is unable to be added
+                    //warn the user that there will be no preview available for this texture
+                    if (!m_texturePreviewHandler.HandleFile(textureFileName))
+                    {
+                        //TODO: Warn user preview not available for this texture
+                    }
+                    //After handling the texture return the trimmed string of the fileName (parent directory / file name without extension) to add to the form texture list box
+                    return FileUtility.GetFileNameWithParentFolder(textureFileName);
+                }
+                //If not handled return empty string
+                return "";
+            }
+            catch (Exception e)
+            {
+                //Data manager will log exception and return an empty string to signal a failed handle of the texture
+                System.Console.WriteLine( e.Message );
+                //Should probably call client Error / Exception / Warning window class to display a proper message to user
+
+                //return empty string
+                return "";
+            }
+        }
+
+        public void LayerSelectionChanged(string layerName)
+        {
+            m_tileLayerHandler.UpdateCurrentLayer(layerName);
+        }
+
+        public void RemoveTexture(string fileNameKey)
+        {
+            //Before removing it from the texture handler, check all layer layouts, if a layer is utilizing this texture
+            //the cells using this texture must be set back to be empty cells, and the texture must be removed from the tile layer texture list
+            if( m_textureHandler.ContainsTexture( fileNameKey ) )
+                m_tileLayerHandler.RemoveTextureFromLayers( m_textureHandler.GetTexture(fileNameKey));
+  
+            m_textureHandler.Remove(fileNameKey);
+            m_texturePreviewHandler.Remove(fileNameKey);
+        }
+
+        public Image GetPreviewImage(string imageFileKey)
+        {
+            Image image = m_texturePreviewHandler.GetImage(imageFileKey);
+
+            if (image != null)
+            {
+                return image;
+            }
+            else
+            {
+                //log error and return a null value, so the preview box won't display anything
+                System.Console.WriteLine("The key " + imageFileKey + " did not match any image values in storage.");
+
+                return null;
+            }
+        }
+        //Getters of general layer data 
+
+        //Get general layer data for the largest selected layer
+        public KeyValuePair<int, int> GetLayerDimensions( List<string> layerNames)
+        {
+            return m_tileLayerHandler.GetLayerDimensions(layerNames);
+        }
+
+        public int GetLayerAlpha(string layerName)
+        {
+            return m_tileLayerHandler.GetLayerAlpha(layerName);
+        }
+
+        //Not sure if this is good design, but here we are allowing the data manager to offer up handles to each of it's handlers
+        public TileLayerHandler TileLayerDataHandle { get { return m_tileLayerHandler; } }
+    }
+}
---
+++ TileEditor/TileEditor/Manager/GraphicsManager.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using TileEditor.Handlers;
+using TileEditor.Controls;
+using TileEngine.Layer;
+
+namespace TileEditor.Manager
+{
+    public class GraphicsManager
+    {
+        //handle to main graphics device
+        private GraphicsDevice m_graphicsDevice;
+        //Graphics manager needs various data for displaying different objects... currently just needs the tile layer handler
+        private DataManager m_dataManager;
+        //Handle to the actual graphics control this manager is managing
+        //in the future, this probably should not be a specific graphics control
+        private GraphicsEditor m_graphicsControl;
+
+        private int m_displayWidth;
+        private int m_displayHeight;
+
+        //The position of the screen inside of the tile map
+        private Vector2 m_screenPosition;
+
+        public GraphicsManager(GraphicsEditor graphicsEditor, DataManager dataManager)
+        {
+            m_graphicsControl = graphicsEditor;
+            m_graphicsDevice = graphicsEditor.GraphicsDevice;
+            m_dataManager = dataManager;
+
+            m_displayWidth = m_graphicsDevice.PresentationParameters.BackBufferWidth;
+            m_displayHeight = m_graphicsDevice.PresentationParameters.BackBufferHeight;
+
+            m_screenPosition = Vector2.Zero;
+        }
+
+        public void Draw( SpriteBatch spriteBatch, List<string> selectedKeys, bool drawTileOutlines )
+        {
+            m_graphicsDevice.Clear(Color.DarkCyan);
+            //We will need handle to the tile layer handler to draw the layer layouts
+            TileLayerHandler tileLayerHandler = m_dataManager.TileLayerDataHandle;
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
+
+            //Only draw selected tile layer layouts
+            foreach (string key in selectedKeys)
+            {
+                if (tileLayerHandler.ContainsLayer(key))
+                {
+                    EditorTileLayer tileLayer = tileLayerHandler.GetLayer(key);
+                    //Tell the tile engine to draw the layer layout, but also let the engine know the position the screen currently
+                    tileLayer.Draw(spriteBatch, m_screenPosition);
+
+                    //Now if tile outlining is checked we draw the tile outline of all empty tile cells 
+                    if (drawTileOutlines)
+                    {
+                        for (int x = 0; x < tileLayer.LayoutWidth; x++)
+                        {
+                            for (int y = 0; y < tileLayer.LayoutHeight; y++)
+                            {
+                                //Is this cell empty?
+                                if (tileLayer.GetCellIndex(x, y) == TileLayer.EMPTYTILEINDEX)
+                                {
+                                    spriteBatch.Draw(m_dataManager.EditorOutlineTexture,
+                                                        new Rectangle(x * TileLayer.TileWidth - (int)m_screenPosition.X, y * TileLayer.TileHeight - (int)m_screenPosition.Y, 
+                                                                        TileLayer.TileWidth, TileLayer.TileHeight),
+                                                                        Color.White);
+                                }
+                            }
+                        }
+                    }
+
+                    //Draw the outline of the cell the user is currently hovering over on the current layer, if there is no current layer
+                    //or the mouse is outside the graphics control nothing will be drawn. The tile layer handler handles whether there is a current layer or not
+                    if (tileLayerHandler.CellX != -1 || tileLayerHandler.CellY != -1)
+                    {
+                        spriteBatch.Draw(m_dataManager.EditorOutlineTexture,
+                                            new Rectangle(tileLayerHandler.CellX * TileLayer.TileWidth - (int)m_screenPosition.X,
+                                                            tileLayerHandler.CellY * TileLayer.TileHeight - (int)m_screenPosition.Y,
+                                                            TileLayer.TileWidth, TileLayer.TileHeight),
+                                            Color.Red);
+                    }
+                }
+            }
+
+            spriteBatch.End();
+        }
+
+        //Update the position of the screen within the layer map, and update the cell the user is currently hovering over
+
+        //ya.. so there's an MonoGame Point, and a System.Drawing Point... we'll just use the System.Drawing Point class
+        public void Update(int hScrollBarValue, int vScrollBarValue, System.Drawing.Point mousePosition )
+        {
+            TileLayerHandler tileLayerHandler = m_dataManager.TileLayerDataHandle;
+
+            m_screenPosition.X = hScrollBarValue * EditorTileLayer.TileWidth;
+            m_screenPosition.Y = vScrollBarValue * EditorTileLayer.TileHeight;
+
+            int mousePosX = m_graphicsControl.PointToClient(mousePosition).X;
+            int mousePosY = m_graphicsControl.PointToClient(mousePosition).Y;
+
+            if (mousePosX >= 0 && mousePosX < m_displayWidth && mousePosY >= 0 && mousePosY < m_displayHeight)
+            {
+                tileLayerHandler.SetCurrentCellX(mousePosX, hScrollBarValue);
+                tileLayerHandler.SetCurrentCellY(mousePosY, vScrollBarValue);
+            }
+            else
+            {
+                tileLayerHandler.SetCellsOutsideBound();
+            }
+        }
+   
+        public void ClearControlScreen()
+        {
+            m_graphicsDevice.Clear(Color.DarkCyan);
+        }
+    }
+}
