3d9b5c5fe172c04d9efd46ef8ef2c97dc5962c6e
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-15 20:08:45 -0500

    Utility Functions

---
+++ TileEditor/TileEditor/Utility/FileUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TileEditor.Utility
+{
+    public static class FileUtility
+    {
+        public static string GetFileNameWithParentFolder(string fileName)
+        {
+            return Directory.GetParent(fileName).Name + "/" + Path.GetFileNameWithoutExtension(fileName);
+        }
+    }
+}
