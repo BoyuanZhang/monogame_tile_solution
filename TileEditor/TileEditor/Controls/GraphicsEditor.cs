3b51bf76468fbc0fef252c8c62475548acab7f49
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-15 20:08:34 -0500

    GL Control used for form

---
+++ TileEditor/TileEditor/Controls/GraphicsEditor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using TileEditor.GLControlSource;
+
+namespace TileEditor.Controls
+{
+    public class GraphicsEditor : GraphicsDeviceControlWrapper
+    {
+        //public event handler to allow the form to handle requests to update and draw 
+        public EventHandler OnDraw;
+
+        private SpriteBatch m_spriteBatch;
+
+        public GraphicsEditor() 
+        { 
+        } 
+
+        protected override void Initialize()
+        {
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            GraphicsDevice.Clear(Color.DarkCyan);
+        }
+
+        protected override void Draw()
+        {
+            if (OnDraw != null)
+                OnDraw(this, null);
+        }
+
+        public SpriteBatch spriteBatch { get { return m_spriteBatch; } }
+    }
+}
