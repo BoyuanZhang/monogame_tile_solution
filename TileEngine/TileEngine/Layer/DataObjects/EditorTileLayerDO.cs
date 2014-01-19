007a9092e778e264707633d751467ac145af1d13
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-19 01:12:49 -0500

    Data access objects

---
+++ TileEngine/TileEngine/Layer/DataObjects/EditorTileLayerDO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TileEngine.Layer;
+
+namespace TileEngine.Layer.DataObjects
+{
+    public class EditorTileLayerDO
+    {
+        private EditorTileLayer m_tileLayer;
+        private List<string> m_layerTextureNameList;
+
+        public EditorTileLayerDO(EditorTileLayer tileLayer, List<string> textureNameList)
+        {
+            m_tileLayer = tileLayer;
+            m_layerTextureNameList = textureNameList;
+        }
+
+        public EditorTileLayer EditorTileLayer { get { return m_tileLayer; } }
+        public List<string> LayerTextureNameList { get { return m_layerTextureNameList; } }
+    }
+}
